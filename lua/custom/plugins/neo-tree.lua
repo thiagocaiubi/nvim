3dccf1f8b25368157eb0df975dce7cd6e6d3be79
Thiago Caiubi <thiago.caiubi@gmail.com>
2023-05-27 17:35:57 -0300

    neo-tree: install file tree

---
+++ lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,16 @@
+-- Unless you are still migrating, remove the deprecated commands from v1.x
+vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
+
+vim.api.nvim_set_keymap('n', '<C-\\>', ':Neotree toggle<CR>', { noremap = true, silent = true })
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v2.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {}
+  end,
+}
